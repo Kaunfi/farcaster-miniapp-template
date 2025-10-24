c9f0e449a76b9d3a1c1da658bbb313c206f8ece2
Kaunfi <166756176+Kaunfi@users.noreply.github.com>
2025-10-24 23:23:33 +0200

    Update contract address in WalletActions component

--- components/Home/WalletActions.tsx
+++ components/Home/WalletActions.tsx
@@ -26,7 +26,7 @@ export function WalletActions() {
   }
 
   // Adresse du contrat déployé
-  const contractAddress = '0xCdcC45169fCbF6cEcC1931f3415978E3a6553ACd'
+  const contractAddress = '0x4f0a2ddb1f4094016566434c592b41a111b19740'
 
   async function handleSend(message: string) {
     try {
